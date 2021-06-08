d235e3509886e1318242106f69d85c97e066d353
Alexey Subbotin <jiehhap@gmail.com>
2021-06-08 16:55:45 +0700

    added note list template

--- app/src/main/java/com/jiehhap/notes/screens/MainFragment.kt
+++ app/src/main/java/com/jiehhap/notes/screens/MainFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.jiehhap.notes.R
 
-class StartFragment : Fragment() {
+class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 }
